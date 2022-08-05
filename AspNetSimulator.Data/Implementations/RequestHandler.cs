b447ca2ac7f29ff1523ec2ff579848a32116cd08
Mrinal Singh <s.mrinalsingh@gmail.com>
2022-08-05 17:35:50 +0530

    handle basic default routing

--- AspNetSimulator.Data/Implementations/RequestHandler.cs
+++ AspNetSimulator.Data/Implementations/RequestHandler.cs
@@ -14,29 +14,34 @@ namespace AspNetSimulator.Data.Implementations
 
         public void Handle(HttpListenerContext context)
         {
-            string? reqController = GetControllerName(context);
-            if (reqController == null)
+            Type? reqController = GetControllerName(context);
+            if (reqController != null)
             {
-                //context.Response.OutputStream.Write(Encoding.UTF8.GetBytes("Not Found: " + reqPath));
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.Close();
-            }
+                string actionName = GetActionName(context);
+                string? retValue = reqController?
+                    .GetMethods(System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public)?
+                    .Where(x => x.Name.Equals(actionName, StringComparison.InvariantCultureIgnoreCase))?
+                    .FirstOrDefault()?
+                    .Invoke(Activator.CreateInstance(reqController), null)?
+                    .ToString();
 
+                    context.Response.OutputStream.Write(Encoding.UTF8.GetBytes("Found Controller: " + reqController + ". Got Value from method: " + retValue));
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    context.Response.Close();
+            }
             else
             {
-                context.Response.OutputStream.Write(Encoding.UTF8.GetBytes("Found Controller: " + reqController));
-                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
                 context.Response.Close();
             }
         }
 
-        private string? GetControllerName(HttpListenerContext context)
+        private Type? GetControllerName(HttpListenerContext context)
         {
             var controllerBase = typeof(IControllerBase);
             var controllers = AppDomain.CurrentDomain.GetAssemblies()
                 .SelectMany(s => s.GetTypes())
                 .Where(p => controllerBase.IsAssignableFrom(p))
-                .Select(c => c.Name)
                 .ToList();
 
             List<string>? reqPath = context.Request?.Url?.AbsolutePath?.ToLower()[1..].Split("/").ToList();
@@ -51,8 +56,22 @@ namespace AspNetSimulator.Data.Implementations
                     curController = reqPath?[0];
             }
 
-            string? foundPath = controllers?.FirstOrDefault(c => c.Contains(curController, StringComparison.CurrentCultureIgnoreCase));
+            Type? foundPath = controllers?.FirstOrDefault(c => c.Name.Contains(curController, StringComparison.CurrentCultureIgnoreCase));
             return foundPath;
+        }
+
+        private string GetActionName(HttpListenerContext context)
+        {
+            List<string> reqPath = context.Request?.Url?.AbsolutePath?.ToLower()[1..].Split("/").ToList();
+            if (reqPath.Count > 1)
+            {
+                if (reqPath[1] == "")
+                    return _routeConfig.DefaultAction.ToLower();
+                else
+                    return reqPath[1];
+            }
+            else
+                return _routeConfig.DefaultAction.ToLower();
         }
     }
 }
